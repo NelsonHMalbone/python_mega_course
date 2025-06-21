8edada654d6bacd321065dd1c22f69a33d5092b1
Nelson Malbone <nhmalbone@icloud.com>
2025-06-21 01:23:26 -0400

    06-21-2025 worked on some of the project with getting user input with prompt storing message in a part with out a variable storing a message.

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("Lessons Learn Tracker")
-print("Enter your lesson Learned:")
-input()
+
+#print("Enter your lesson Learned:")
+prompt_user_text = "Enter your lesson Learned: " # variable to store message
+user_text = input(prompt_user_text)
+
+#print("Add a quick note (optional): ")
+# no prompt to store message
+optional_note = input("Add a quick note (optional): ")
+
+print(user_text)
+print(optional_note)
