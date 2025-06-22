3fd6a682a29293c8dd91b83b2fe62c72278c2c29
Nelson Malbone <nhmalbone@icloud.com>
2025-06-22 00:43:32 -0400

    06-22-2025 finshed up section 1.8 and 1.9 of module 1 added additional inputs for the user to input multiple lessons and then store them into a python list but in the next part we will get rid of the mulitple inputs in use a while loop to ask user to add more or to quit

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ print("Lessons Learn Tracker")
 prompt_user_text = "Enter your lesson Learned: " # variable to store message
 user_text = input(prompt_user_text)
 
+# adding more inputs
+user_text2 = input(prompt_user_text)
+user_text3 = input(prompt_user_text)
+
 #print("Add a quick note (optional): ")
 # no prompt to store message
 optional_note = input("Add a quick note (optional): ")
 
-print(user_text)
-print(optional_note)
+lessons = [user_text, user_text2, user_text3]
+
+print(lessons)
+#print(user_text)
+#print(optional_note)
