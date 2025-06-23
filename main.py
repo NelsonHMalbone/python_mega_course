bae9b9dea8824c6d8673c66d0195cec054641311
Nelson Malbone <nhmalbone@icloud.com>
2025-06-23 13:59:47 -0400

    06-23-2025 setting up a way for asking for input over and over using a while loop. and added notes also.
    hid the optional question for now.

--- main.py
+++ main.py
@@ -2,18 +2,8 @@ print("Lessons Learn Tracker")
 
 #print("Enter your lesson Learned:")
 prompt_user_text = "Enter your lesson Learned: " # variable to store message
-user_text = input(prompt_user_text)
 
-# adding more inputs
-user_text2 = input(prompt_user_text)
-user_text3 = input(prompt_user_text)
+while True:
+    user_text = input(prompt_user_text)
+    # optional_note = input("Add a quick note (optional): ")
 
-#print("Add a quick note (optional): ")
-# no prompt to store message
-optional_note = input("Add a quick note (optional): ")
-
-lessons = [user_text, user_text2, user_text3]
-
-print(lessons)
-#print(user_text)
-#print(optional_note)
