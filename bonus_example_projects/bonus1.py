c00d9a69cb550b12b825e5f47252638fe24279b2
Nelson Malbone <nhmalbone@icloud.com>
2025-06-23 13:45:15 -0400

    06-23-2025 Completed a bonus example project something easy and simple in the course it wanted to do boook titles but i decide to do game titles.
    working on section 2 of the app 1 build

---
+++ bonus_example_projects/bonus1.py
@@ -0,0 +1,8 @@
+# game title counter
+
+game_title_input = input("enter a game title here: ").title()
+
+length_of_title = len(game_title_input)
+
+print(f'The Game Title {game_title_input} length is {length_of_title}')
+
