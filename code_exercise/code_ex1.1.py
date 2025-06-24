17eb168421505f5efcfbe7c87b64ca62b7883f9a
Nelson Malbone <nhmalbone@icloud.com>
2025-06-24 14:42:11 -0400

    06-24-2025 new dictionary folder for code exercises code exercises to allow user or learner to begin short and easy projects

---
+++ code_exercise/code_ex1.1.py
@@ -0,0 +1,8 @@
+# while loop and user input
+# sectin 2.24
+
+
+
+while True:
+    user_name = input("what is your name: ")
+    print(user_name.title())
---
+++ code_exercise/code_ex1.py
@@ -0,0 +1,6 @@
+# code exercise 1 infinite while loop
+# section 2.23
+
+user_name = input("What is your name: ")
+while True:
+    print(user_name.title())
