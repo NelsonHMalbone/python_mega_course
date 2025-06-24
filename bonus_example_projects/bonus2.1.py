c8433e3e8c66b4ae6bd036085a0e7d30086f7637
Nelson Malbone <nhmalbone@icloud.com>
2025-06-24 14:35:02 -0400

    06-24-2025 got two bonus projects completed working with conditions != and <=
    working with append methods.
    
    updated the notes file as well

---
+++ bonus_example_projects/bonus2.1.py
@@ -0,0 +1,6 @@
+# start with a value
+x = 1
+
+while x <= 6:
+    print(x)
+    x = x + 1
--- main.py
+++ main.py
@@ -3,7 +3,10 @@ print("Lessons Learn Tracker")
 #print("Enter your lesson Learned:")
 prompt_user_text = "Enter your lesson Learned: " # variable to store message
 
+# list method
+lesson_list = []
+
 while True:
     user_text = input(prompt_user_text)
     # optional_note = input("Add a quick note (optional): ")
-
+    lesson_list.append(user_text)
