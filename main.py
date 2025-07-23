c67f8268af80c2ee915eda22850d6a7de06d6770
Nelson Malbone <nhmalbone@icloud.com>
2025-07-22 22:15:24 -0400

    07-22-2025 working with files adding a file into the same dictionary so that the main can see and read file. added a function to save the user input into the txt file and so it saves and does not reset everytime a user inputs a new line

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ prompt_user_text = "Enter your lesson Learned: " # variable to store message
 prompt_user_text1 = "Would you like to add, show, edit, complete, exit (select one):  "
 
 # list method
-lesson_list = []
+# lesson_list = [] # dont need this because we are reading file now
 
 while True:
     user_decision = input(prompt_user_text1).strip()
     user_decision = user_decision.strip()
     match user_decision:
         case "add"|"a":
-            user_text = input(prompt_user_text)
+            user_text = input(prompt_user_text) + '\n'
+            # read file to bebale to see the in console
+            file = open('lesson_learn.txt', 'r')
+            # equalivant to wrieline but just reading
+            lesson_list = file.readlines()
+            # need to close file so different code cant affect file
+            file.close()
             # optional_note = input("Add a quick note (optional): ")
             lesson_list.append(user_text.capitalize())
-
+            file = open('lesson_learn.txt','w')
+            file.writelines(lesson_list)
+            file.close()
         case "show"|"s":
             for index, item in enumerate(lesson_list):
                 row = f'{index + 1}:{item}' # starts list at 1
@@ -28,7 +36,7 @@ while True:
 
         case "complete":
             number = int(input("number of lesson to that are completed: "))
-            removed = lesson_list.pop(number - 1) # removes the number selected 
+            removed = lesson_list.pop(number - 1) # removes the number selected
             print(f'out of lesson list {removed} was removed')
 
         case "exit"|"x":
