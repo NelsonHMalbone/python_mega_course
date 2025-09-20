b771c1538002671dc331f0a2d434b6128d529b59
Nelson Malbone <nhmalbone@icloud.com>
2025-09-20 05:47:56 -0400

    fixed some spelling mistakes
    do not know how to get rid of the yellow line by the lesson list in the edit part of the user selection

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ while True:
                 lessons_new = item.strip("\n")
                 new_lesson_list.append(lessons_new)
 
-            # lsit comperhension way
+            # list comparison  way
             # new_lesson_list = [item.strip("\n") for item in lessons]
 
             for index, item in enumerate(new_lesson_list):
