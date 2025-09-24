63aea46beaf46fcb2d0a4d357ae4d904b2c4e886
Nelson Malbone <nhmalbone@icloud.com>
2025-09-24 15:06:25 -0400

    9-24 added an additional time input to the journal app that was from yesterdays bonus code project

--- bonus_example_projects/journal_app.py
+++ bonus_example_projects/journal_app.py
@@ -1,8 +1,10 @@
 date = input("Enter the date (ex.2020-20-09): ")
+time = input("Enter the time (24 Hr or 12 Hr): ")
 mood_rating = input("Enter your mood 1 - 10: ")
 thoughts = input("Let the thoughts flow:\n")
 
 # creating a file using the date as the filename
 with open(f"../bonus_example_projects/Journal/{date}.txt", "w") as file:
+    file.write(time + "\n")
     file.write(mood_rating + "\n")
     file.write(thoughts)
