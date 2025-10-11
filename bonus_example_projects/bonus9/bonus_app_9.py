5f64a1910bb3636345a401570b43a8dfa345d5c6
Nelson Malbone <nhmalbone@icloud.com>
2025-10-11 19:31:06 -0400

    10-11 using dictionaries to have keys and values i prefer using this method

--- bonus_example_projects/bonus9/bonus_app_9.py
+++ bonus_example_projects/bonus9/bonus_app_9.py
@@ -6,15 +6,15 @@ from unicodedata import digit
 password_input = input("Enter new password: ")
 
 # result list for password
-result = []
+result = {}
 
 # checking against conditions
 # chars 8 or greater
 
 if len(password_input) >= 8:
-    result.append(True)
+    result["length: "] = True
 else:
-    result.append(False)
+    result["length: "] = False
 
 # checking if number is in string
 digit = False
@@ -22,7 +22,7 @@ for i in password_input:
     if i.isdigit():
         digit = True
 
-result.append(digit)
+result["digit: "] = digit
 
 # checking to see if there is a uppercase char
 upper_char = False
@@ -30,15 +30,17 @@ for x in password_input:
     if x.isupper():
         upper_char = True
 
-result.append(upper_char)
+result["upper: "] = upper_char
 
 # getting the strong or weak password
 # using all function
 
-if all(result):
+print(result)
+
+if all(result.values()):
     print("Strong Password")
 else:
     print("Weak password")
 
-# at this point we are finshed but now we are going to switch it all over to dictionaries
-# so now we can see whats true and not true
+# at this point we are finished, but now we are going to switch it all over to dictionaries
+# so now we can see what's true and not true
