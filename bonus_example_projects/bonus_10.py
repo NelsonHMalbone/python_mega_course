7ff1f0681dec79351d076f01a493173bf4374b2e
Nelson Malbone <nhmalbone@icloud.com>
2025-10-18 04:58:44 -0400

    10-11 section 10 bonus project

--- bonus_example_projects/bonus_10.py
+++ bonus_example_projects/bonus_10.py
@@ -1,1 +1,10 @@
 # creating a area caluclator
+try:
+    length = float(input("enter a length: "))
+    width = float(input("enter a width: "))
+
+    area = length * width
+
+    print(area)
+except ValueError:
+    print("Please enter a number")
