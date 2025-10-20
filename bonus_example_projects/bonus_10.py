fbf14a1f83938cc19767f8948fc3bd5da74aecc2
Nelson Malbone <nhmalbone@icloud.com>
2025-10-20 14:42:43 -0400

    10-11 section 10 bonus project using a if statement to run the code as if there was a error if user input a square number

--- bonus_example_projects/bonus_10.py
+++ bonus_example_projects/bonus_10.py
@@ -2,9 +2,11 @@
 try:
     length = float(input("enter a length: "))
     width = float(input("enter a width: "))
+    if length == width:
+        print("This is a square try again")
+    else:
+        area = length * width
 
-    area = length * width
-
-    print(area)
+        print(area)
 except ValueError:
     print("Please enter a number")
