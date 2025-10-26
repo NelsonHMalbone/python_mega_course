23b264c21f051e8196c149f64befb4751a12bd36
Nelson Malbone <nhmalbone@icloud.com>
2025-10-25 21:38:27 -0400

    10-25 instead of having the return have a long sentence we only return meters. know as decouple. we want return to be short not long create a function to take feet and inches to meters.

---
+++ bonus_example_projects/bonus_12.py
@@ -0,0 +1,16 @@
+feet_inches = input("enter feet and inches: ")
+
+def convert(feet_inches):
+    split_convert = feet_inches.split(" ")
+    feet = float(split_convert[0])
+    inch = float(split_convert[1])
+
+    meters = feet * 0.3048 + inch * 0.0254
+    return meters 
+
+result = convert(feet_inches)
+
+if result < 1:
+    print("Kid to small")
+else:
+    print("kid can use the slide")
