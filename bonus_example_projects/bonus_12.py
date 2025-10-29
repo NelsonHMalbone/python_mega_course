03b7d30a0459657fcd549a951d292e1ec30da473
Nelson Malbone <nhmalbone@icloud.com>
2025-10-29 14:03:26 -0400

    10-26 we split the main function into two seperate functions so the one functions wasnt doing three differet jobs basically

--- bonus_example_projects/bonus_12.py
+++ bonus_example_projects/bonus_12.py
@@ -1,14 +1,24 @@
+# using this for bonus 13 also
+
 feet_inches = input("enter feet and inches: ")
 
-def convert(feet_inches):
+# in bonus 13 we discover this function is doing to much doing more then one thing
+# does the parsing and conversion
+
+def parse(feet_inches):
     split_convert = feet_inches.split(" ")
     feet = float(split_convert[0])
     inch = float(split_convert[1])
+    return feet, inch
 
+def convert(feet, inch):
     meters = feet * 0.3048 + inch * 0.0254
     return meters
 
-result = convert(feet_inches)
+feet_inches_tuple = parse(feet_inches)
+print("fi", feet_inches_tuple)
+result = convert(feet_inches_tuple[0], feet_inches_tuple[1])
+
 
 if result < 1:
     print("Kid to small")
