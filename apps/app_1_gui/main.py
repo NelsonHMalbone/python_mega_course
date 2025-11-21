82491f059b923e8a26fd0a7a91cc250fae46c304
Nelson Malbone <nhmalbone@icloud.com>
2025-11-21 15:09:29 -0500

    11-21 created a basic gui without interactions

---
+++ apps/app_1_gui/main.py
@@ -0,0 +1,10 @@
+from utils_gui import get_lesson, write_lesson
+import FreeSimpleGUI as sg
+#freesimplegui 5.0.0 or above
+label = sg.Text("Type in a lesson: ")
+input_box = sg.InputText(tooltip="Enter lesson")
+button_input = sg.Button("Enter Lesson")
+
+window = sg.Window("My lessons List", layout=[[label, input_box, button_input]])
+window.read()
+window.close()
---
+++ apps/app_1_gui/utils_gui.py
@@ -0,0 +1,21 @@
+FILEPATH = "apps/app_1/lesson_learn.txt"
+
+def get_lesson(filepath=FILEPATH):
+    """read a text file i n return the
+    list of lesson items
+    """
+    # will use the filepath that is show in add,show, and edit
+    with open(filepath, 'r') as file_local:
+        # equal to writeline but just reading
+        lesson_list_local = file_local.readlines()  # list was created here
+    return lesson_list_local
+
+# to write to files
+# does not need a variable to be called unlike the get lesson
+def write_lesson(lesson_arg, filepath=FILEPATH):
+    """write a lesson item list in the text file"""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(lesson_arg)
+
+if __name__ == "__main__":
+    print()
