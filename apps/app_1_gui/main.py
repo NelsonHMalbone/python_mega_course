be76a61c0f4141ad0024bb70ff6c6150eb0b2c89
Nelson Malbone <nhmalbone@icloud.com>
2025-11-25 14:57:04 -0500

    11-25 learned how to change the font and size

--- apps/app_1_gui/main.py
+++ apps/app_1_gui/main.py
@@ -6,6 +6,8 @@ input_box = sg.InputText(tooltip="Enter lesson")
 button_input = sg.Button("Enter Lesson")
 quit_button = sg.Button("Quit")
 
-window = sg.Window("My lessons List", layout=[[label, input_box],[quit_button, button_input]])
+window = sg.Window("My lessons List",
+                   layout=[[label, input_box],[quit_button, button_input]],
+                   font=('Helvetica', 15))
 window.read()
 window.close()
