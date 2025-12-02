1c73b4d08aa20cd86c40b06d0ee6d88c6e302ec6
Nelson Malbone <nhmalbone@icloud.com>
2025-12-02 11:41:40 -0500

    12-02 update with a edit button and a functional one also. also put in a list box to allow user to select a lesson to be edited

--- apps/main_gui.py
+++ apps/main_gui.py
@@ -6,9 +6,15 @@ input_box = sg.InputText(tooltip="Enter lesson", key="lesson_key")
 button_input = sg.Button("Enter")
 quit_button = sg.Button("Quit")
 edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values=get_lesson(),
+                      key='lesson_box',
+                      enable_events=True,
+                      size=[45,10])
 
 window = sg.Window("My lessons List",
-                   layout=[[label, input_box],[quit_button, button_input, edit_button]],
+                   layout=[[label, input_box],
+                           [quit_button, button_input, edit_button],
+                           [list_box]],
                    font=('Helvetica', 15))
 # prevent program from closing when pushing the enter button
 
@@ -16,12 +22,27 @@ while True:
     event, values = window.read()
     print(event)
     print(values)
+    print(values["lesson_box"])
     match event:
         case "Enter":
             lesson_list = get_lesson()
             new_lessons = values["lesson_key"] + "\n"
             lesson_list.append(new_lessons)
             write_lesson(lesson_list)
+
+        case "Edit":
+            # getting lesson user selected 0 is to get the string
+            lesson_to_edit = values["lesson_box"][0] # new lesson
+            new_lessons = values['lesson_key'] # lesson to be edited
+
+            # getting current todos
+            lessons = get_lesson()
+            index = lessons.index(lesson_to_edit)
+            lessons[index] = new_lessons
+            write_lesson(lessons)
+
+            # to update list box in real time
+            window['lesson_box'].update(values=lessons)
 
         # prevents a error message when closing out app before you get the "quit" button to work
         #case sg.WIN_CLOSED:
