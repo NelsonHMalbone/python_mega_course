4b0db914b3827cb02eea2414d2a4125c78069e9d
Nelson Malbone <nhmalbone@icloud.com>
2025-12-16 13:26:17 -0500

    12-16 made the input box add the new lesson or todo to the txt file using onchange and a key

--- apps/web.py
+++ apps/web.py
@@ -3,10 +3,19 @@ import utils
 
 lessons = utils.get_lesson()
 
+def add_value_lesson():
+    new_lesson = st.session_state["new_lessons"] + "\n"
+    print(new_lesson)
+    lessons.append(new_lesson)
+    utils.write_lesson(lessons)
+
+
 st.title("My Todo Web App")
 st.write("This app is to increase productivity")
 
 for lesson in lessons:
     st.checkbox(lesson)
 
-st.text_input(label= "Enter a new lesson: ")
+st.text_input(label= "Enter a new lesson: ",
+              on_change=add_value_lesson,
+              key="new_lessons")
