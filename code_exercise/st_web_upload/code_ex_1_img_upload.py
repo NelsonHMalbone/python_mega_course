c18a9a34fad240c8a7d52f28ae947ecc90aa02f5
Nelson Malbone <nhmalbone@icloud.com>
2025-12-26 00:31:56 -0500

    12-25 we finished up the convert photo for the uploaded img. also solved the problem of it showing the double pic with removing st.image its was acting like a print statement

--- code_exercise/st_web_upload/code_ex_1_img_upload.py
+++ code_exercise/st_web_upload/code_ex_1_img_upload.py
@@ -2,6 +2,16 @@ import streamlit as st
 from PIL import Image
 import io
 
+def download_photo():
+    # convert img to bytes
+    buf = io.BytesIO()
+    convert_grayscale.save(buf, format="PNG")
+    st.image(buf)
+
+    st.download_button(label="Download Image",
+                       data=buf.getvalue(),
+                       file_name="gray_img.png",
+                       mime="image/png")
 
 st.title("Picture Upload Convertor")
 
@@ -9,19 +19,16 @@ with st.expander("start camera"):
 
     cam_to_be_taken = st.camera_input("Camera")
 
-    if cam_to_be_taken:
+    if cam_to_be_taken is not None:
         open_im = Image.open(cam_to_be_taken)
         convert_grayscale = open_im.convert("L")
-        st.image(convert_grayscale)
+        download_photo()
 
-        # convert img to bytes
-        buf = io.BytesIO()
-        convert_grayscale.save(buf, format="PNG")
-        st.image(buf)
 
-        st.download_button(label="Download Image",
-                           data=buf.getvalue(),
-                           file_name="gray_img.png",
-                           mime="image/png")
+with st.expander("upload a picture"):
+    upload_photo = st.file_uploader("upload a picture")
 
-st.expander("upload a picture")
+    if upload_photo is not None:
+        open_photo = Image.open(upload_photo)
+        convert_grayscale = open_photo.convert("L")
+        download_photo()
